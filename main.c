ab8698d077fd37818704ffc45e4eff2e485209fd
kjurczyn <01169180@pw.edu.pl>
2023-06-05 17:43:53 +0200

    added allegro5 dlls

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <allegro5/allegro5.h>
 #include <allegro5/keyboard.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
 
-//RCX, RDX, R8, R9, stack
 extern void lissajous_draw(void* pixel_array, const unsigned int pitch, const unsigned int half_length, const double w1, const double w2, const double d); 
 
 
@@ -35,7 +35,7 @@ int main()
         return 1;
     }
 
-    ALLEGRO_DISPLAY* disp = al_create_display(701, 701);
+    ALLEGRO_DISPLAY* disp = al_create_display(702, 728);
     if(!disp)
     {
         printf("couldn't initialize display\n");
@@ -52,6 +52,7 @@ int main()
     ALLEGRO_COLOR black = al_map_rgb(0, 0, 0);
     ALLEGRO_COLOR red = al_map_rgb(255, 0, 0);
 
+    ALLEGRO_FONT* font = al_create_builtin_font();
 
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
@@ -145,7 +146,9 @@ int main()
                 lissajeou_changed = false;
             }
             al_clear_to_color(black);
-            al_draw_scaled_bitmap(lissajeous_bitmap, 0,0, 100, 100, 0, 0, 701, 701, 0);
+            al_draw_textf(font, white, 0, 710, 0, "W1: %f", w1);
+            al_draw_textf(font, white, 0, 720, 0, "W2: %f", w2);
+            al_draw_scaled_bitmap(lissajeous_bitmap, 0,0, 100, 100, 0, 0, 700, 700, 0);
             al_flip_display();
             redraw = false;
         }
